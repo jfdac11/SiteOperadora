baf4804d030bcc6cccd40f26b849284c8dd01b79
Ana Tartari <anatartari412@gmail.com>
2021-01-22 15:45:24 -0300

    Projeto

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ul>
+          <li>Nome Fantasia: Brasil Saude</li>
+          <li>Registro ANS: 42115-4</li>
+          <li>CNPJ: 28.633.372/0001-74</li>
+          <li>Razão Social: LUNA CARE ASSISTÊNCIA MÉDICA LTDA</li>
+          <li>Situação do Registro ANS: Ativa</li>
+          <li>Total de Consumidores: 3</li>
+          <li>Tipo: Operadora médico-hospitalar</li>
+          <li>Modalidade: Medicina de Grupo</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
